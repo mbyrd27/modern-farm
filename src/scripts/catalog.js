7628361f1d58f4ec7d7e377a156bfbd8d3bd4d0e
Matt Byrd <mbyrd7886@outlook.com>
2020-12-10 22:12:28 -0500

    refactored catalog.js to use .map()

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,12 +1,10 @@
 export const Catalog = (food) => {
     let contentTarget = document.querySelector('.container');
     
-    food.forEach(item => {
-        let catalogItem = `<section class="food">
-        <h2>${item.type}</h2>
-        </section>
-        `
-        contentTarget.innerHTML += catalogItem;
-    });
+    contentTarget.innerHTML += food.map(item => {
+        return `<section class="food">
+                    <h2>${item.type}</h2>
+                </section>`
+    }).join('');
 
 }
